51fbbf43bacbbf6d9cbd2e7483c8498f691b6d9d
Alex Motenko <tea.alex@gmail.com>
2016-04-11 06:41:22 +0200

    unique url mechanism, csv exporting

--- playainfo.js
+++ playainfo.js
@@ -1,118 +1,155 @@
-EventsCollection = new Mongo.Collection("events");
-ProvidersCollection = new Mongo.Collection("providers");
+EventsCollection = new Mongo.Collection('events');
+ProvidersCollection = new Mongo.Collection('providers');
+
+function getProviderUuidByUrl() {
+  uuid = '';
+  try {
+    uuid = FlowRouter.getParam('uuid');
+  } catch (e) {
+    log('Attempted to extract provider uuid from invalid URL');
+  }
+  return uuid;
+}
+
+function getProvider() {
+  uuid = getProviderUuidByUrl();
+  result = ProvidersCollection.findOne({'uuid': uuid});
+  return result;
+}
 
 if (Meteor.isClient) {
-
-  Meteor.startup(function () {
+  Meteor.startup(function() {
   });
-  
-  Template.event.helpers({
-    formatTime: function(rawTime) {
-      return moment(rawTime).format();
-    }
+
+  Template.registerHelper('formatTime', function(rawTime) {
+    return moment(rawTime).format();
   });
 
   Template.viewEventsPage.events({
-    "click .eventDeleteButton": function () {
-      Meteor.call("deleteEvent", this._id);
+    'click .eventDeleteButton': function() {
+      Meteor.call('deleteEvent', this._id);
     },
 
-    "click .eventDeleteAllButton": function () {
-      Meteor.call("deleteAllEvents");
-    }
+    'click .eventDeleteAllButton': function() {
+      Meteor.call('deleteAllProviderEvents', this.uuid);
+    },
   });
-  
+
   Template.viewEventsPage.helpers({
-    eventsCollection: function() {
-      return EventsCollection.find({}, {sort: {start: -1}});
-    }
+    provider: function() {
+      return getProvider();
+    },
+
+    events: function() {
+      // uuid = FlowRouter.getParam('uuid');
+      // TODO remove
+      // if (uuid === '0') {
+      //   return EventsCollection.find({}, {sort: {start: -1}});
+      // }
+      return EventsCollection.find({'uuid': this.uuid}, {sort: {start: -1}});
+    },
   });
 
   Template.viewProvidersPage.events({
-    "click .providerDeleteButton": function () {
-      Meteor.call("deleteProvider", this._id);
+    'click .providersDeleteButton': function() {
+      Meteor.call('deleteProvider', this.uuid);
     },
 
-    "click .providerDeleteAllButton": function () {
-      Meteor.call("deleteAllProviders");
-    }
+    'click .providersDeleteAllButton': function() {
+      Meteor.call('deleteAllProviders');
+    },
+
+    'click .providersExport': async function() {
+      csv = await Meteor.callPromise('exportProviders');
+      blob = new Blob([csv], {type: 'text/plain;charset=utf-8'});
+      // TODO add new Date() to filename
+      saveAs(blob, 'providers.csv');
+    },
   });
 
   Template.viewProvidersPage.helpers({
-    providersCollection: function() {
+    providers: function() {
       return ProvidersCollection.find({}, {sort: {start: -1}});
-    }
+    },
   });
 
-
   Template.addProvidersPage.events({
-    "submit .addProvider": function (formEvent) {
+    'submit .addProvider': function(formEvent) {
       formEvent.preventDefault();
 
-      Meteor.call("addProvider", {
+      Meteor.call('addProvider', {
         hebrewName: formEvent.target.hebrewName.value,
         englishName: formEvent.target.englishName.value,
-        type: formEvent.target.typeSelector.value
+        type: formEvent.target.typeSelector.value,
       });
 
-      formEvent.target.hebrewName.value = "";
-      formEvent.target.englishName.value = "";
-    }
+      formEvent.target.hebrewName.value = '';
+      formEvent.target.englishName.value = '';
+    },
+  });
+
+  Template.addEventPage.helpers({
+    provider: function() {
+      return getProvider();
+    },
   });
 
   Template.addEventPage.events({
-    "submit .addEvent": function (formEvent) {
+    'submit .addEvent': function(formEvent) {
       formEvent.preventDefault();
 
-      Meteor.call("addEvent", {
+      Meteor.call('addEvent', {
         title_hebrew: formEvent.target.hebrew_title.value,
         titleText: formEvent.target.english_title.value,
         titleDesc: formEvent.target.english_description.value,
         start: formEvent.target.start_date.value,
-        end: formEvent.target.end_date.value
-
+        end: formEvent.target.end_date.value,
+        uuid: FlowRouter.getParam('uuid'),
       });
 
-      formEvent.target.hebrew_title.value = "";
-      formEvent.target.english_title.value = "";
-      formEvent.target.english_description.value = "";
-      formEvent.target.start_date.value = "";
-      formEvent.target.end_date.value = "";
-    }
+      formEvent.target.hebrew_title.value = '';
+      formEvent.target.english_title.value = '';
+      formEvent.target.english_description.value = '';
+      formEvent.target.start_date.value = '';
+      formEvent.target.end_date.value = '';
+    },
   });
 }
 
 if (Meteor.isServer) {
-  // Meteor.publish("EventsCollectionSub", function () {
+  // Meteor.publish('EventsCollectionSub', function() {
   //   return EventsCollection.find();
   // });
 }
 
 Meteor.methods({
-  addEvent: function (event) {
+  addEvent: function(event) {
     EventsCollection.insert({
       title: event.titleText,
       title_hebrew: event.title_hebrew,
       description: event.titleDesc,
       start: new Date(event.start),
       end: new Date(event.end),
-      createdAt: new Date() // current time
+      uuid: event.uuid,
+      createdAt: new Date(),
     });
   },
 
-  deleteEvent: function (eventId) {
+  deleteEvent: function(eventId) {
     EventsCollection.remove(eventId);
   },
 
-  deleteAllEvents: function () {
+  deleteAllProviderEvents: function(providerUuid) {
+    EventsCollection.remove({'uuid': providerUuid});
+  },
+
+  deleteProvider: function(providerUuid) {
+    EventsCollection.remove({'uuid': providerUuid});
+    ProvidersCollection.remove({'uuid': providerUuid});
+  },
+
+  deleteAllProviders: function() {
     EventsCollection.remove({});
-  },
-
-  deleteProvider: function (eventId) {
-    ProvidersCollection.remove(eventId);
-  },
-
-  deleteAllProviders: function () {
     ProvidersCollection.remove({});
   },
 
@@ -121,7 +158,20 @@ Meteor.methods({
       name: provider.englishName,
       nameHebrew: provider.hebrewName,
       type: provider.type,
-      createdAt: new Date() // current time
+      createdAt: new Date(),
+      uuid: uuid.v4(),
     });
-  }
+  },
+
+  exportEvents: function() {
+    col = EventsCollection.find().map(function(u) {return u;});
+    csv = Papa.unparse(col);
+    return csv;
+  },
+
+  exportProviders: function() {
+    col = ProvidersCollection.find().map(function(u) {return u;});
+    csv = Papa.unparse(col);
+    return csv;
+  },
 });
